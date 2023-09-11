f17f1ea14c4cc1b5eb300060be6621a00fffd46c
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-09-11 09:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let counter = {};
+	for(int i = 0; i<str.length; i++){
+		if(str[i] in counter){
+			counter.str[i]++;
+		}
+		else{
+			counter.str[i] = 1;
+		}
+	}
+	for (let i = 0; i < str.length; i++) {
+		if(counter.str[i] == 1){
+			return str[i];
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
